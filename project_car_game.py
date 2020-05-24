b7425c1baf16a40182ada1cce638434cba12d493
GaddamMS <madhusudhangaddam@live.com>
2020-05-24 19:10:27 +0530

    update project_car_game after watching solution

--- project_car_game.py
+++ project_car_game.py
@@ -1,16 +1,19 @@
 options = ''
 
-while options.lower() != 'quit':
-    options = input('>')
-    if options.lower() == 'start':
+while True:
+    options = input('>').lower()
+    if options == 'start':
         print('Car started ..')
-    elif options.lower() == 'stop':
+    elif options == 'stop':
         print('Car stopped ..')
-    elif options.lower() == 'help':
+    elif options == 'help':
         print('start - to start the car')
         print('stop - to stop the car')
         print('quit - to exit')
-    elif options.lower() == 'quit':
+    elif options == 'quit':
         break
     else:
         print("I don't understand that...")
+
+#After the solution, reformatting the program by streamlining .lower func to input
+#while statement changed
