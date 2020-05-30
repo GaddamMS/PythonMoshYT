b1f93443da5b7096d92703fda718ad7057fd12b5
GaddamMS <madhusudhangaddam@live.com>
2020-05-30 21:52:41 +0530

    update project_car_game after watching solution

--- project_car_game.py
+++ project_car_game.py
@@ -1,11 +1,21 @@
 options = ''
-
+is_started = False
+is_stopped = False
 while True:
     options = input('>').lower()
     if options == 'start':
-        print('Car started ..')
+        if is_started is False:
+            print('Car started ..')
+            is_started = True
+        elif is_started is True:
+            print('Car already started')
     elif options == 'stop':
-        print('Car stopped ..')
+        if is_stopped is False:
+            print('Car stopped ..')
+            is_stopped = True
+        elif is_stopped is True:
+            print('Car already stopped')
+
     elif options == 'help':
         print('start - to start the car')
         print('stop - to stop the car')
